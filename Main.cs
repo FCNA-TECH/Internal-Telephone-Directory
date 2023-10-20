ea32a0cfad9dceb0bd6e883f1cdde9b67e3e2a3c
Ferdinand <97032094+FCNA-TECH@users.noreply.github.com>
2023-10-20 10:50:11 +0100

    Update Main.cs
    File creation and reading all work
    
    Improved the formatting of the displayed strings
    Made some minor changes to variable names 
    Added employee index numbers when being displayed

--- Main.cs
+++ Main.cs
@@ -27,6 +27,7 @@ class Program
         case "4":
           break;
         case "5":
+          getmeoutofhere = 1;
           break;
       }
       if(getmeoutofhere == 1){
@@ -42,21 +43,51 @@ class Program
     StreamWriter writer = new StreamWriter(stream);
     Console.WriteLine("How many people? ");
     int HowMany = Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine(" ");
     for (int Count = 1; Count <= HowMany; Count++)
     {
+      Contact employee = new Contact();
+      
       Console.WriteLine("Enter Name: ");
       string Name1 = Console.ReadLine();
+      employee.Name = Name1;
 
       Console.WriteLine("Enter Number: ");
       string Number1 = Console.ReadLine();
+      employee.Number = Number1;
 
-      Console.WriteLine("Enter Name: ");
+      Console.WriteLine("Enter JobTitle: ");
       string JobTitle1 = Console.ReadLine();
+      employee.JobTitle = JobTitle1;
+
+      writer.WriteLine(employee.Name);
+      writer.WriteLine(employee.Number);
+      writer.WriteLine(employee.JobTitle);
+      Console.WriteLine("");
+      Console.WriteLine("Employee Saved! ");
+      Console.WriteLine("");
     }
+    writer.Close();
   }
+  
   public static void DisplayFile()
   {
-
+    FileStream stream = File.Open("MyDatabase.txt", FileMode.Open);
+    StreamReader reader = new StreamReader(stream);
+    Contact employee = new Contact();
+    int i = 1;
+    while(!reader.EndOfStream){
+      employee.Name = reader.ReadLine();
+      employee.Number = reader.ReadLine();
+      employee.JobTitle = reader.ReadLine();
+      Console.WriteLine(" ");
+      Console.WriteLine("{0}. Emplyee Name: {1}",i, employee.Name);
+      Console.WriteLine("{0}. Emplyee Number: {1}",i, employee.Number);
+      Console.WriteLine("{0}. Emplyee JobTitle: {1}",i, employee.JobTitle);
+      Console.WriteLine(" ");
+      i++;
+    }
+    reader.Close();
   }
   public static void SearchFile()
   {
@@ -64,14 +95,16 @@ class Program
   }
   public static void AppendFile()
   {
-
+    FileStream stream = File.Open("MyDatabase.txt", FileMode.Append);
+    StreamWriter writer = new StreamWriter(stream);
   }
   public static string DisplayMenu()
   {
+    Console.WriteLine(" ");
     Console.WriteLine("=========MENU=========");
     Console.WriteLine("1. Create Directory: ");
     Console.WriteLine("2. Display Directory: ");
-    Console.WriteLine("3. Find # using name: "); //find the number using name was shortened to # for aesthetics
+    Console.WriteLine("3. Find # using name: ");
     Console.WriteLine("4. Append Directory: ");
     Console.WriteLine("5. Exit Program: ");
     Console.Write("enter either 1 - 5:  ");
